7fcc0cb3b7b04a1083e91ca9347941d46cd93335
MBARKI2001 <nouhmbarki31@gmail.com>
2025-03-12 13:24:39 +0100

    Add files via upload
    Ajout de la Base de Données, nécessaire pour le projet

---
+++ maisonligue.sql
@@ -0,0 +1,168 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 12 mars 2025 à 13:23
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `maisonligue`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `faq`
+--
+
+CREATE TABLE `faq` (
+  `id_faq` int(11) NOT NULL,
+  `question` text NOT NULL,
+  `reponse` text NOT NULL,
+  `dat_question` datetime NOT NULL,
+  `dat_reponse` datetime NOT NULL,
+  `id_user` bigint(11) NOT NULL,
+  `status` enum('1','2') NOT NULL DEFAULT '1',
+  `dat_modif` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Déclencheurs `faq`
+--
+DELIMITER $$
+CREATE TRIGGER `before_update_faq` BEFORE UPDATE ON `faq` FOR EACH ROW BEGIN
+    SET NEW.dat_modif = NOW();
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ligue`
+--
+
+CREATE TABLE `ligue` (
+  `id_ligue` bigint(11) NOT NULL,
+  `lib_ligue` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` bigint(11) NOT NULL,
+  `pseudo` varchar(20) NOT NULL,
+  `mdp` varchar(255) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `id_usertype` bigint(11) NOT NULL,
+  `id_ligue` bigint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `usertype`
+--
+
+CREATE TABLE `usertype` (
+  `id_usertype` bigint(11) NOT NULL,
+  `lib_usertype` varchar(50) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `faq`
+--
+ALTER TABLE `faq`
+  ADD PRIMARY KEY (`id_faq`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Index pour la table `ligue`
+--
+ALTER TABLE `ligue`
+  ADD PRIMARY KEY (`id_ligue`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `id_usertype` (`id_usertype`),
+  ADD KEY `id_ligue` (`id_ligue`);
+
+--
+-- Index pour la table `usertype`
+--
+ALTER TABLE `usertype`
+  ADD PRIMARY KEY (`id_usertype`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `faq`
+--
+ALTER TABLE `faq`
+  MODIFY `id_faq` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `ligue`
+--
+ALTER TABLE `ligue`
+  MODIFY `id_ligue` bigint(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` bigint(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `usertype`
+--
+ALTER TABLE `usertype`
+  MODIFY `id_usertype` bigint(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `faq`
+--
+ALTER TABLE `faq`
+  ADD CONSTRAINT `faq_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Contraintes pour la table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_usertype`) REFERENCES `usertype` (`id_usertype`),
+  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`id_ligue`) REFERENCES `ligue` (`id_ligue`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
